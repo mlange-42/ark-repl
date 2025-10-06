a60dee25846f9afe6ef3cb951a5bbedb8b224687
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2025-10-06 15:03:43 +0200

    Add an example for a custom command (#20)

---
+++ examples/custom-cmd/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/mlange-42/ark-repl/examples"
+	"github.com/mlange-42/ark-repl/repl"
+	"github.com/mlange-42/ark/ecs"
+)
+
+// customCommand with arguments.
+type customCommand struct {
+	Text string `default:"Nothing" help:"Text to print."`
+	N    int    `default:"1" help:"Number of repetitions."`
+}
+
+func (c customCommand) Execute(repl *repl.Repl, out *strings.Builder) {
+	for range c.N {
+		fmt.Fprintln(out, c.Text)
+	}
+}
+
+func (c customCommand) Help(repl *repl.Repl, out *strings.Builder) {
+	fmt.Fprintln(out, "A custom command.")
+}
+
+func main() {
+	world := ecs.NewWorld(32)
+
+	// Populate the world so there is something to see.
+	examples.Populate(&world)
+
+	pause := true
+	stop := false
+
+	// Callbacks for loop control.
+	callbacks := repl.Callbacks{
+		Pause: func(out *strings.Builder) {
+			pause = true
+		},
+		Resume: func(out *strings.Builder) {
+			pause = false
+		},
+		Stop: func(out *strings.Builder) {
+			stop = true
+		},
+		Ticks: func() int {
+			return ecs.GetResource[examples.Tick](&world).Tick
+		},
+	}
+
+	repl := repl.NewRepl(&world, callbacks)
+
+	// Add the custom command:
+	repl.AddCommand("custom", customCommand{})
+
+	// For control from this terminal:
+	repl.Start()
+
+	// For control from another terminal:
+	//repl.StartServer(":9000")
+
+	// Update loop.
+	for {
+		// Execute incoming REPL commands.
+		repl.Poll()
+
+		if stop { // Stopped?
+			break
+		}
+		if pause { // Paused?
+			time.Sleep(50 * time.Millisecond)
+			continue
+		}
+
+		// Update step
+		examples.Update(&world)
+		ecs.GetResource[examples.Tick](&world).Tick++
+		// Emulate frame time
+		time.Sleep(50 * time.Millisecond)
+	}
+}
